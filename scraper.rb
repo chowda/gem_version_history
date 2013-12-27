c1a139fd9064459f1c1578b56161ca4c8f8ce6a2
Daniel Crowder <daniel.crowder@bizbuilt.com>
2013-12-27 11:09:49 -0800

    Being smarter about IO. This is much faster.

--- scraper.rb
+++ scraper.rb
@@ -6,23 +6,27 @@ require 'active_support/core_ext'
 
 def main
 	gem_list = get_remote_list()
+	total_gems = gem_list.size
 	if gem_list.empty?
 		raise "No remote gems found!"
 	else
-		p "Total gems found from remote: #{gem_list.size}"
+		p "Total gems found from remote: #{total_gems}"
 	end
 
-	write_history_headers()
-	gem_list.each do |g|
+	version_holder = []
+	current_position = 1
+	gem_list[0,100].each do |g|
+		current_position += 1
+		p "on # #{g}| (#{(current_position / total_gems.to_f) * 100}%)" if current_position % 10 == 0
 		begin
-			p "Starting on gem #{g}"
 			page = Nokogiri::HTML(open("http://rubygems.org/gems/#{g}/versions"))
-			versions = parse_version_history(page, g)
-			write_history(versions)
+			version_holder << versions = parse_version_history(page, g)
 		rescue Exception => e
 			p "Failed to parse gem #{g}. Exception: #{e}"
 		end
 	end
+
+	write_history(version_holder)
 end
 
 def get_remote_list
@@ -68,15 +72,12 @@ end
 
 def write_history(versions)
   CSV.open("gem_history.csv", "ab") do |csv|
-  	versions.each do |v|
-	  	csv << v
+  	csv << ['name', 'version number', 'date', 'size']
+  	versions.each do |version|
+  		version.each do |v|
+	  		csv << v
+	  	end
 	  end
-	end
-end
-
-def write_history_headers
-	CSV.open("gem_history.csv", "wb") do |csv|
-		['name', 'version number', 'date', 'size']
 	end
 end
 
